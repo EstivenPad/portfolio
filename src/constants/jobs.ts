61f78888a9773c614282cf25420c2ea41192a586
Estiven Padilla Santos <estivenps26@gmail.com>
2025-06-09 15:31:25 -0400

    fixing some breaking changes related to tailwind v4

--- src/constants/jobs.ts
+++ src/constants/jobs.ts
@@ -8,9 +8,18 @@ interface Job {
 
 export const JOBS:Job[] = [
     {
+        role: 'Full-Stack Developer',
+        company: 'XpertCode',
+        duration: 'MAR 2025 - PRESENT',
+        url: 'https://xpertcode.com.do/',
+        responsabilities: [
+            'Work at Claro Dominicana a telecommunications company as contractor, contributing to the Claro Dominicana E-Commerce project.'
+        ]
+    },
+    {
         role: 'Software Engineer',
         company: 'Universidad Católica Nordestana (UCNE)',
-        duration: 'SEP 2021 - PRESENT',
+        duration: 'SEP 2021 - MAR 2025',
         url: 'https://www.ucne.edu/p/',
         responsabilities: [
             'Develop new features, resolve existing issues, and maintenance legacy code regarding with the existent systems.',
